f53605c245e76b29e7788be1059fd801e1e95aa8
dhsudev <lua@gnomo.net>
2025-04-19 21:53:14 +0200

    fix: navigation now loading the pages

--- app/src/main/java/com/example/apilist/ui/navigation/NavWapper.kt
+++ app/src/main/java/com/example/apilist/ui/navigation/NavWapper.kt
@@ -1,26 +1,29 @@
 package com.example.apilist.ui.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.example.apilist.ui.screens.FavScreen
+import com.example.apilist.ui.screens.ListScreen
+import com.example.apilist.ui.screens.SettingsScreen
 import com.example.apilist.ui.navigation.Destinations.ListScreen
 import com.example.apilist.ui.navigation.Destinations.FavScreen
 import com.example.apilist.ui.navigation.Destinations.SettingsScreen
 
 
 @Composable
-fun NavWrapper(navController: NavHostController) {
-    NavHost(navController, ListScreen) {
-        composable<ListScreen> {
-
+fun NavWrapper(navController: NavHostController, modifier: Modifier) {
+    NavHost(navController, ListScreen, modifier = modifier) {
+        composable<Destinations.ListScreen> {
+            ListScreen()
         }
-        //TODO: resta de rutes
-        composable<FavScreen> {
-            //TODO: càrrega del composable amb paràmetres (si en té)
+        composable<Destinations.FavScreen> {
+            FavScreen()
         }
-        composable<SettingsScreen> {
-            //TODO: càrrega del composable amb paràmetres (si en té)
+        composable<Destinations.SettingsScreen> {
+            SettingsScreen()
         }
     }
 }
