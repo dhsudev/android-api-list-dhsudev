3b802c207926995a8d3293dbd1bb169a4c0577bb
dhsudev <lua@gnomo.net>
2025-04-19 22:00:03 +0200

    feat(ui): started settingScreen

--- app/src/main/java/com/example/apilist/ui/screens/SettingsScreen.kt
+++ app/src/main/java/com/example/apilist/ui/screens/SettingsScreen.kt
@@ -10,7 +10,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun SettingsScreen(navigateToNext: (String, String) -> Unit) {
+fun SettingsScreen() {
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -20,5 +20,4 @@ fun SettingsScreen(navigateToNext: (String, String) -> Unit) {
         ) {
         Text("Settings Screen")
     }
-
 }
